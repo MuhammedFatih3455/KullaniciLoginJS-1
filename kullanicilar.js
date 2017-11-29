25f65e9b44e17e8cb28cffaad889aa240c4fea15
VoRTeX <vv_vortex_vv@hotmail.com>
2017-11-29 11:11:06 +0300

    Oluşturuldu

---
+++ kullanicilar.js
@@ -0,0 +1,41 @@
+﻿[
+    {
+        "Id":"0",
+        "kullanici_adi":"te.ori",
+        "sifre": "123456",
+        "isim": "Orhan",        
+       "soyisim": "Aygün",
+       "profil_resmi": "orhan.jpg",
+       "d_tarihi": "01.01.1985",
+       "mezun_oldgu_okul": "Gazi Ünv.",
+       "fakulte": "ESEF",
+       "ozgeçmiş": "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit.. Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum. Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum.",
+       "konular":["C#","javascript","html"]
+    },
+    {
+       "Id":"1",
+       "kullanici_adi": "VoRTeX",
+       "sifre": "321987",
+       "isim": "Cumali",
+       "soyisim": "Besli",
+       "profil_resmi": "cumali.jpg",
+       "d_tarihi": "22.11.1984",
+       "mezun_oldgu_okul": "Bartın Üniversitesi",
+       "fakulte": "Bilgisayar Programcılığı",
+       "ozgeçmiş": "Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum. Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum.. Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum",
+       "konular": ["C#", "javascript", "html","T-SQL","Asp.NET MVC"]
+    },
+    {
+        "Id": "2",
+        "kullanici_adi": "MuhammedFatih3455",
+        "sifre": "000000",
+        "isim": "Muhammed Fatih",
+        "soyisim": "Yavuz",
+        "profil_resmi": "fatih.jpg",
+        "d_tarihi": "08.01.1988",
+        "mezun_oldgu_okul": "Namık Kemal Unv.",
+        "fakulte": "Mühendislik Fakultesi",
+        "ozgeçmiş": "Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum... Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum.. Quia dolor sit amet, consectetur, adipisci velit, neque porro quisquam est qui dolorem ipsum",
+        "konular": ["C#", "javascript", "html", "T-SQL", "Asp.NET MVC"]
+    }
+]
